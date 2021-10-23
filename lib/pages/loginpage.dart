6750afd3ac86ce723c7d9adee3184981e2317418
noyon103 <abubakkarnoyon@gmail.com>
2021-10-23 16:45:33 +0600

    day7 comlete

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,87 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:nayan/pages/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          SizedBox(
-              width: 300,
-              height: 200,
-              child: Image.asset(
-                "assets/images/login.png",
-                fit: BoxFit.fill,
-              )),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text("Creative BD",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold)),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter your user ID",
-                    labelText: "User ID",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter your Password",
-                    labelText: "Password",
-                  ),
-                ),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+                width: 300,
+                height: 200,
+                child: Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.fill,
+                )),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
+            const Text("Creative BD",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter your user ID",
+                      labelText: "User ID",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter your Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
 
-          InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, MyRoutes.homeRoute);
-            },
-            child: Container(
-              width: 100,
-              height: 50,
-              alignment: Alignment.center,
-              child: Text(
-                "LOGIN",
-                style: TextStyle(color: Colors.white),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.purpleAccent,
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  changeButton = true;
+                });
+                await Future.delayed(Duration(seconds: 1));
+
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                width: 100,
+                height: 50,
+                alignment: Alignment.center,
+                child: changeButton
+                    ? Icon(
+                        Icons.done,
+                        color: Colors.white,
+                      )
+                    : Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                decoration: BoxDecoration(
+                  // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(changeButton ? 30 : 8),
+                  color: Colors.purpleAccent,
+                ),
               ),
             ),
-          ),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-          //   },
-          //   child: Text("Login"),
-          //   style: TextButton.styleFrom(
-          //       backgroundColor: Colors.red, minimumSize: const Size(100, 40)),
-          // ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text("Forget Password?"),
-        ],
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+            //   },
+            //   child: Text("Login"),
+            //   style: TextButton.styleFrom(
+            //       backgroundColor: Colors.red, minimumSize: const Size(100, 40)),
+            // ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text("Forget Password?"),
+          ],
+        ),
       ),
     );
   }
